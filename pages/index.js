5c245b8f671fc8aae99c817faacea225fb88cce4
mkristian <mkristian@live.ru>
2022-05-11 07:04:52 -0800

    Added mint handler

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
+import Image from 'next/image'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import Web3 from 'web3'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [account, setAccount] = useState()
+  const [account, setAccount] = useState(null)
+  const { ethereum } = window;
   useEffect(() => {
     const connectWalletHandler = async () => {
-      const { ethereum } = window;
       if (ethereum) {
         try {
           const [accounts] = await ethereum.request({ method: 'eth_requestAccounts' })
@@ -20,9 +21,29 @@ export default function Home() {
     }
     connectWalletHandler()
   }, [])
+
+  const mintftHandler = async () => {
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+       
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className={styles.container}>
-      Account: {account}
+      <h1>Account: {account}</h1>
+      <div>
+        <button onClick={mintftHandler}>
+          Buy
+        </button>
+      </div>
+      <br />
+      {account && <Image src='/nft.jpg' width={400} height={400} />}
     </div>
   )
 }
