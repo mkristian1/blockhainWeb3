5f86152f7d927d1dacabe41af3f11f75da8398cc
mkristian <mkristian@live.ru>
2022-05-11 05:34:19 -0800

    Added metamask

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Web3 from 'web3'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [account, setAccount] = useState()
+  useEffect(() => {
+    const connectWalletHandler = async () => {
+      const { ethereum } = window;
+      if (ethereum) {
+        try {
+          const [accounts] = await ethereum.request({ method: 'eth_requestAccounts' })
+          setAccount(accounts)
+          console.log(accounts, 'account');
+        } catch {
+          console.log('No authorized');
+        }
+      }
+    }
+    connectWalletHandler()
+  }, [])
   return (
     <div className={styles.container}>
-    dsf
+      Account: {account}
     </div>
   )
 }
